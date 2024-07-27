ed00bb27bc58e38d8f814ea1717579a63066cbc8
Markus Fisch <mf@markusfisch.de>
2024-07-27 15:03:01 +0200

    Add sound fx
    Using the awesome Zuper Zmall Zound Zynth from Frank Force
    for the sound effects (https://github.com/KilledByAPixel/ZzFX).

--- src/src.js
+++ src/src.js
@@ -240,6 +240,7 @@ function gameLost() {
 	gameOver = now
 	player.alive = false
 	shake()
+	zzfx(...[5,0,129.81,.02,.34,.26,1,1.7,,-0.1,,.01,,,,,,.8,.17,,781])
 }
 
 function offset(x, y) {
@@ -286,6 +287,7 @@ function clearAdjacentWalls(x, y) {
 }
 
 function impact(x, y) {
+	zzfx(...[1.8,,59,.01,.03,.04,3,3.5,,-3,,,,1.4,,.2,.04,.65,.02])
 	shake()
 	spawnDust(x, y)
 	setItem(x, y, BLOCK)
@@ -371,6 +373,7 @@ function digestItem(e) {
 		if (e === player) {
 			spawnDust(player.x, player.y, 4)
 			gameLost()
+			return
 		} else {
 			return
 		}
@@ -387,6 +390,7 @@ function digestItem(e) {
 		break
 	}
 	items[o] = 0
+	zzfx(...[,,584,.02,.17,.36,,2,,,53,.06,.09,,,.2,,.9,.25])
 }
 
 function setDestination(e, x, y) {
@@ -598,6 +602,7 @@ function clearWallAt(x, y) {
 	}
 	score += 1
 	items[o] = pickItem()
+	zzfx(...[1.2,,428,,.09,.16,4,1.8,-1,,,,,.2,,.5,,.48,.01,,1123])
 	spawnDust(x, y, 4)
 	shake()
 }
@@ -1263,3 +1268,20 @@ window.onload = function() {
 	}
 	waitForAtlas(createAtlas(sources))
 }
+
+let // ZzFXMicro - Zuper Zmall Zound Zynth - v1.3.1 by Frank Force ~ 1000 bytes
+zzfxV=.3,               // volume
+zzfxX=new AudioContext, // audio context
+zzfx=                   // play sound
+(p=1,k=.05,b=220,e=0,r=0,t=.1,q=0,D=1,u=0,y=0,v=0,z=0,l=0,E=0,A=0,F=0,c=0,w=1,m=0,B=0
+,N=0)=>{let M=Math,d=2*M.PI,R=44100,G=u*=500*d/R/R,C=b*=(1-k+2*k*M.random(k=[]))*d/R,
+g=0,H=0,a=0,n=1,I=0,J=0,f=0,h=N<0?-1:1,x=d*h*N*2/R,L=M.cos(x),Z=M.sin,K=Z(x)/4,O=1+K,
+X=-2*L/O,Y=(1-K)/O,P=(1+h*L)/2/O,Q=-(h+L)/O,S=P,T=0,U=0,V=0,W=0;e=R*e+9;m*=R;r*=R;t*=
+R;c*=R;y*=500*d/R**3;A*=d/R;v*=d/R;z*=R;l=R*l|0;p*=zzfxV;for(h=e+m+r+t+c|0;a<h;k[a++]
+=f*p)++J%(100*F|0)||(f=q?1<q?2<q?3<q?Z(g**3):M.max(M.min(M.tan(g),1),-1):1-(2*g/d%2+2)
+%2:1-4*M.abs(M.round(g/d)-g/d):Z(g),f=(l?1-B+B*Z(d*a/l):1)*(f<0?-1:1)*M.abs(f)**D*(a<
+e?a/e:a<e+m?1-(a-e)/m*(1-w):a<e+m+r?w:a<h-c?(h-a-c)/t*w:0),f=c?f/2+(c>a?0:(a<h-c?1:(h
+-a)/c)*k[a-c|0]/2/p):f,N?f=W=S*T+Q*(T=U)+P*(U=f)-Y*V-X*(V=W):0),x=(b+=u+=y)*M.cos(A*
+H++),g+=x+x*E*Z(a**5),n&&++n>z&&(b+=v,C+=v,n=0),!l||++I%l||(b=C,u=G,n=n||1);p=zzfxX.
+createBuffer(1,h,R);p.getChannelData(0).set(k);b=zzfxX.createBufferSource();
+b.buffer=p;b.connect(zzfxX.destination);b.start()}
